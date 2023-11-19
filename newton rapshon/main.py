dc117cfbc4d457c10c142dd33ac8f1d60c6d7ca6
mir <129966369+mir-yilmaz@users.noreply.github.com>
2023-11-19 22:50:15 +0300

    Update main.py

--- newton rapshon/main.py
+++ newton rapshon/main.py
@@ -6,14 +6,16 @@ def f(x):
 def f_turev(x):
     return -2 * math.exp(-0.5 * x) - 1
 
-def newton(x0, iterasyon):
-    for i in range(iterasyon):
+x0 = 2
+
+def newton(x0, max_iterasyon):
+    for i in range(max_iterasyon):
         x0 = x0 - f(x0) / f_turev(x0)
         print(f"{i + 1}. iterasyondaki kök: {x0}")
     return x0
 
 
-x0 = 2
-iterasyon = 4
 
-print("kök:", newton(x0, iterasyon))
+max_iterasyon = 4
+
+print("kök:", newton(x0, max_iterasyon))
