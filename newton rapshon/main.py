82799a916d5ffe2c0f0bf6aa45391f11b262a458
mir <129966369+mir-yilmaz@users.noreply.github.com>
2023-11-19 22:48:43 +0300

    Update main.py

--- newton rapshon/main.py
+++ newton rapshon/main.py
@@ -1,19 +1,19 @@
 import math
 
-
 def f(x):
-    return (4 * math.exp(-0.5 * x)) - x
+    return 4 * math.exp(-0.5 * x) - x
 
 def f_turev(x):
-    return (-2 * math.exp(-0.5 * x)) - 1
+    return -2 * math.exp(-0.5 * x) - 1
 
 def newton(x0, iterasyon):
     for i in range(iterasyon):
-        x1 = x0 - f(x0) / f_turev(x0)
-        x0 = x1
-    return x1
+        x0 = x0 - f(x0) / f_turev(x0)
+        print(f"{i + 1}. iterasyondaki kök: {x0}")
+    return x0
+
 
 x0 = 2
 iterasyon = 4
-kok = newton(x0, iterasyon)
-print("Kök: ",kok)
+
+print("kök:", newton(x0, iterasyon))
